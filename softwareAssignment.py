1363089063883ec161021e67b8d936aa32d99d7a
Jonas Elflein <joel00001@stud.uni-saarland.de>
2022-04-08 15:48:04 +0200

    Initial commit

---
+++ softwareAssignment.py
@@ -0,0 +1,62 @@
+
+'''
+Introduction to Python Programming (aka Programmierkurs I, aka Python I)
+Software Assignment
+'''
+
+import sys
+
+class SearchEngine:
+
+    def __init__(self, collectionName, create):
+        '''
+        Initialize the search engine, i.e. create or read in index. If
+        create=True, the search index should be created and written to
+        files. If create=False, the search index should be read from
+        the files. The collectionName points to the filename of the
+        document collection (without the .xml at the end). Hence, you
+        can read the documents from <collectionName>.xml, and should
+        write / read the idf index to / from <collectionName>.idf, and
+        the tf index to / from <collectionName>.tf respectively. All
+        of these files must reside in the same folder as THIS file. If
+        your program does not adhere to this "interface
+        specification", we will subtract some points as it will be
+        impossible for us to test your program automatically!
+        '''
+        pass
+    
+    
+      def executeQuery(self, queryTerms):
+        '''
+        Input to this function: List of query terms
+
+        Returns the 10 highest ranked documents together with their
+        tf.idf-sum scores, sorted score. For instance,
+
+        [('NYT_ENG_19950101.0001', 0.07237004260325626),
+         ('NYT_ENG_19950101.0022', 0.013039249597972629), ...]
+
+        May be less than 10 documents if there aren't as many documents
+        that contain the terms.
+        '''
+        pass
+        
+    def executeQueryConsole(self):
+        '''
+        When calling this, the interactive console should be started,
+        ask for queries and display the search results, until the user
+        simply hits enter.
+        '''
+        pass
+    
+if __name__ == '__main__':
+    '''
+    write your code here:
+    * load index / start search engine
+    * start the loop asking for query terms
+    * program should quit if users enters no term and simply hits enter
+    '''
+    # Example for how we might test your program:
+    # Should also work with nyt199501 !
+    searchEngine = SearchEngine("nytsmall", create=True)
+    print(searchEngine.executeQuery(['hurricane', 'philadelphia']))
